0c62bfe62bb084f7e919d3960d9ba149035b6f95
Débora-Karine-Cassimiro <61765035+deboraKa@users.noreply.github.com>
2024-11-17 13:09:52 -0300

    Update Relátório de Evendos e Cursos.sql
    incluindo mais consultas

--- Relátório de Evendos e Cursos.sql
+++ Relátório de Evendos e Cursos.sql
@@ -1,3 +1,7 @@
+--exclui a View antiga 
+DROP VIEW IF EXISTS Relatorio_Eventos_Cursos;
+
+-- Criando a View - Relatorio_Eventos_Cursos
 CREATE VIEW Relatorio_Eventos_Cursos AS
 SELECT
     c.nome AS cliente_nome,
@@ -11,20 +15,33 @@ JOIN Eventos e ON p.evento_id = e.evento_id
 LEFT JOIN Inscricoes i ON i.cliente_id = c.cliente_id
 LEFT JOIN Cursos cu ON i.curso_id = cu.curso_id;
 
+
+--Verificar todos os registros do relatório:
 SELECT * FROM Relatorio_Eventos_Cursos;
 
--- Verificar relatórios apenas para um cliente específico
+
+--Filtrar para um cliente específico:
 SELECT * FROM Relatorio_Eventos_Cursos
 WHERE cliente_nome = 'João Silva';
 
--- Verificar relatórios para eventos no mês de janeiro de 2025
+--Filtrar para eventos em janeiro de 2025:
 SELECT * FROM Relatorio_Eventos_Cursos
 WHERE data_participacao BETWEEN '2025-01-01' AND '2025-01-31';
 
--- Contagem de clientes por evento
+-- Contar a quantidade de participantes por evento:
 SELECT evento_nome, COUNT(cliente_nome) AS quantidade_participantes
 FROM Relatorio_Eventos_Cursos
 GROUP BY evento_nome;
 
 
+-- Verificar todos os registros no Relatório
+SELECT * FROM Relatorio_Eventos_Cursos;
+
+-- Verificar apenas os clientes que participaram de um evento sem curso
+SELECT * FROM Relatorio_Eventos_Cursos
+WHERE curso_nome IS NULL;
+
+-- Verificar apenas os clientes que se inscreveram em um curso sem evento
+SELECT * FROM Relatorio_Eventos_Cursos
+WHERE evento_nome IS NULL;
 
