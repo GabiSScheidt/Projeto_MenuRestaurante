392b16ad0e13082e6cb298fb2e6524dee4299609
Gabriela Scheidt <gabriela.scheidt@yahoo.com>
2024-11-19 21:20:42 -0300

    ajuste final do carossel

--- JS/X.js
+++ JS/X.js
@@ -1,24 +1,32 @@
 let count = 1;
-  document.getElementById ("radio1").checked = true;
+document.getElementById ("radio1").checked = true;
 
 setInterval(function(){
-  nextImage();
-  }, 5000)
+    nextImage();
+}, 5000)
 
 function nextImage(){
-  count++;
-  if(count >3){
-  count = 1;
-  }
-
-document.getElementById ("radio" + count).checked = true;
+    count++;
+    if(count >3){
+        count = 1;
     }
 
-function toggleMenu() {
-  var menu = document.getElementById('menu');
-  if (menu.style.display === 'block') {
-      menu.style.display = 'none';
-  } else {
-      menu.style.display = 'block';
+    document.getElementById ("radio" + count).checked = true;
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
